63ea5c6dbd8f66d271feee0ff9a73e2a5cf4ea2b
arielg <Yelush0207>
2018-11-12 17:16:39 +0200

    merge conf commit

--- HelloWorld/src/com/example/helloworld/HelloWorld.java
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("src");
         System.out.println("first change");
-        System.out.println("second change");
+        System.out.println("second change?");
     }
 }
