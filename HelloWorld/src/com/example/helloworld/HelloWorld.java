c6144b0a39a4bc978120cbb7855a9ac3612cb584
arielg <Yelush0207>
2018-11-12 16:25:20 +0200

    first commit

--- HelloWorld/src/com/example/helloworld/HelloWorld.java
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -3,5 +3,6 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("src");
+        System.out.println("first change");
     }
 }
